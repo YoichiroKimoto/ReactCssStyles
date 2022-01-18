f46d06ae41428b3e51c3831ccd9a82ead484b4b9
YoichiroKimoto <64562711+YoichiroKimoto@users.noreply.github.com>
2022-01-18 12:54:09 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { CssModules } from "./Components/CssModules";
+import { InlineStyle } from "./Components/InlineStyle";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+      <CssModules />
+    </div>
+  );
+}
